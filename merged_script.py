dff5338924f9ba6b38e45cfd57916fc9f313d7df
Sebastian Reyes <a01663071@tec.mx>
2023-11-28 15:47:48 -0600

    Nuevo script, borrar el viejo

---
+++ merged_script.py
@@ -0,0 +1,108 @@
+import time
+import mysql.connector
+from smbus2 import SMBus
+from bme280 import BME280
+
+import RPi.GPIO as GPIO
+from gpiozero import DistanceSensor
+import threading
+
+# Configuración de la conexión a la base de datos MySQL
+db_host = "retocarritocloud.czosggbj7hr7.us-east-1.rds.amazonaws.com"
+db_user = "admin"
+db_password = "8ik9FiE2AgE*"
+db_name = "micarrito"
+
+
+#Temperatura------------------------------------
+
+# Configuración del sensor de temperatura
+bus = SMBus(1)
+bme280 = BME280(i2c_dev=bus)
+
+# Función para insertar la temperatura en la base de datos
+def insertar_temperatura(temperatura):
+    try:
+        conexion = mysql.connector.connect(host=db_host, user=db_user, password=db_password, database=db_name)
+        cursor = conexion.cursor()
+        query = "INSERT INTO carrito (temperatura) VALUES (%s);"
+        cursor.execute(query, (temperatura,))
+        conexion.commit()
+        cursor.close()
+        conexion.close()
+    except Exception as e:
+        print("Error al insertar en la base de datos:", e)
+
+def read_temperature():
+    while True:
+        temperatura = bme280.get_temperature()
+        print('{:05.2f}*C'.format(temperatura))
+        insertar_temperatura(temperatura)
+        time.sleep(0.1)  # Adjust as needed
+
+
+#Rover------------------------------------
+def insertar_distancia(distancia):
+    try:
+        conexion = mysql.connector.connect(host=db_host, user=db_user, password=db_password, database=db_name)
+        cursor = conexion.cursor()
+        query = "INSERT INTO carrito (distancia) VALUES (%s);"
+        cursor.execute(query, (distancia,))
+        conexion.commit()
+        cursor.close()
+        conexion.close()
+    except Exception as e:
+        print("Error al insertar en la base de datos:", e)
+
+def init():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(17, GPIO.OUT)
+    GPIO.setup(22, GPIO.OUT)
+    GPIO.setup(23, GPIO.OUT)
+    GPIO.setup(24, GPIO.OUT)
+
+def start_forward():
+    init()
+    GPIO.output(17, True)
+    GPIO.output(22, False)
+    GPIO.output(23, False) 
+    GPIO.output(24, True)
+
+def start_reverse():
+    init()
+    GPIO.output(17, False)
+    GPIO.output(22, True)
+    GPIO.output(23, True) 
+    GPIO.output(24, False)
+
+def stop():
+    GPIO.cleanup()
+
+def read_distance():
+    sensor = DistanceSensor(echo=9, trigger=10)
+    while True:
+        cm = sensor.distance * 100
+        insertar_distancia(cm)  # Insertar distancia en la base de datos
+
+        if cm > 15:
+            start_forward()
+        else:
+            start_reverse()
+
+        time.sleep(0.1)  # Ajustar para mayor o menor sensibilidad
+
+
+def main():
+    temperature_thread = threading.Thread(target=read_temperature)
+    distance_thread = threading.Thread(target=read_distance)
+
+    temperature_thread.start()
+    distance_thread.start()
+
+    temperature_thread.join()
+    distance_thread.join()
+
+if __name__ == '__main__':
+    main()  
+
+
